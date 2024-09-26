815d579f9d7b386a7879eee92d859300131a73e1
gc-user <aarfi.miracle@gmail.com>
2024-09-26 12:03:31 +0530

    page added for contract between hascode and equals method.

--- src/main/java/com/app/constant/AppConstant.java
+++ src/main/java/com/app/constant/AppConstant.java
@@ -3,5 +3,6 @@ package com.app.constant;
 public class AppConstant {
   private AppConstant(){}
   public  static final String ADVANCE_CONCEPT_VIEW_LOCATION="advance-concept/";
+  public  static final String CORE_JAVA_VIEW_LOCATION="core-java/";
   public static final String ERROR_PAGE_VIEW_NAME ="/error";
 }
--- src/main/java/com/app/controller/ViewController.java
+++ src/main/java/com/app/controller/ViewController.java
@@ -6,13 +6,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import static com.app.constant.AppConstant.ADVANCE_CONCEPT_VIEW_LOCATION;
+import static com.app.constant.AppConstant.CORE_JAVA_VIEW_LOCATION;
+
 @Controller
 @Slf4j
 public class ViewController {
 
     @GetMapping("/advance-concept/{pageName}")
     public String getView(@PathVariable String pageName) {
-        log.info("returning the view page {}", pageName);
-        return AppConstant.ADVANCE_CONCEPT_VIEW_LOCATION + pageName;
+        log.info("returning the view page from advance-concept {}", pageName);
+        return ADVANCE_CONCEPT_VIEW_LOCATION + pageName;
     }
+
+    @GetMapping("/core-java/{pageName}")
+    public String getCoreJavaView(@PathVariable String pageName) {
+        log.info("returning core java page {}", pageName);
+        return CORE_JAVA_VIEW_LOCATION + pageName;
+    }
+
 }
